bc2522e5181c4ff6b0139ed1077ff6d0e18db836
bartoszkruba <bartosz.kruba@gmail.com>
2019-03-22 10:22:23 +0100

    Added fields to new account window

--- SwedenBank/src/JavaFX/controllers/NewAccountController.java
+++ SwedenBank/src/JavaFX/controllers/NewAccountController.java
@@ -1,4 +1,32 @@
 package JavaFX.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
 public class NewAccountController {
+
+   @FXML
+   private TextField accountNameTextField;
+
+   @FXML
+   private Label accountNameError;
+
+   @FXML
+   private CheckBox savingAccountCheckBox;
+
+
+   public TextField getAccountNameTextField() {
+      return accountNameTextField;
+   }
+
+   public Label getAccountNameError() {
+      return accountNameError;
+   }
+
+   public CheckBox getSavingAccountCheckBox() {
+      return savingAccountCheckBox;
+   }
 }
